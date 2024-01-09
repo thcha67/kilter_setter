ffb027ebd55c354b445c51b92cdc420d6b5ed2d2
thcha67 <thcha67@ulaval.ca>
2024-01-09 00:33:18 -0500

    app mod

--- src/android_app/app.py
+++ src/android_app/app.py
@@ -2,6 +2,8 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
+import numpy as np
+import cv2
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -9,14 +11,10 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner
 from kivy.uix.image import Image
-from kivy.uix.scatter import Scatter
-from kivy.uix.scatterlayout import ScatterLayout
-from kivy.uix.scatter import ScatterPlane
 from kivy.uix.button import Button
 from kivy.graphics import *
 
 from scripts.kilter_utils import grade_translations, angle_translations, color_translations, get_all_holes_12x12, get_matrix_from_holes
-
 
 class MyApp(App):
     def __init__(self, **kwargs):
@@ -53,51 +51,43 @@ class MyApp(App):
         top_bar2.add_widget(button3)
 
         # # Create the image below the top bar
-        points = get_all_holes_12x12()
-        image_with_points = KilterImageAllPoints(points=points)
+        background_image = Image(source='src/android_app/assets/kilterboard_background.png', fit_mode="scale-down")
+        self.create_image(background_image)
+        points_image = Image(source='src/android_app/assets/kilterboard_background_all_holes.png', fit_mode="scale-down")
+        
 
         # Add widgets to the main layout
         layout.add_widget(top_bar1)
         layout.add_widget(top_bar2)
-        layout.add_widget(image_with_points)
+        layout.add_widget(points_image)
+        layout.add_widget(background_image)
 
         return layout
 
+    def create_image(self, background_image):
+        """
+        Creates the image of the KilterBoard with the holds
+        """
+        width, height = background_image.size
 
-class KilterImageAllPoints(BoxLayout):
-    def __init__(self, points=None, **kwargs):
-        super(KilterImageAllPoints, self).__init__(**kwargs)
-    
-        self.image = Image(source='src/android_app/assets/kilterboard_background.png', fit_mode="scale-down")
-        self.add_widget(self.image)
+        image = np.zeros((height, width, 4), np.uint8)
+        image[:, :, 3] = 255
 
-        self.points = points
+        all_holes = get_all_holes_12x12()
 
-        self.bind(pos=self.update_canvas, size=self.update_canvas)
-        self.bind(pos=self.update_shapes, size=self.update_shapes)
+        for x, y, color in all_holes:
+            color = hex_to_rgb(color_translations[color])
+            cv2.circle(image, (x, y), 10, color, -1)
 
-    def update_canvas(self, instance, value):
-        # Clear previous canvas instructions and draw transparent background
-        self.canvas.before.clear()
-        with self.canvas.before:
-            Color(1, 1, 1, 0)  # Set the color with alpha 0 for a transparent background
-            Ellipse(pos=self.pos, size=self.size)
+        cv2.imwrite('src/android_app/assets/kilterboard_background_all_holes.png', image)
 
-    def update_shapes(self, instance, value):
-        # Clear previous canvas instructions and draw shapes
-        self.canvas.after.clear()
-        with self.canvas.after:
-            for x, y, color in self.points:
-                Color(*hex_to_rgb(color_translations[color]))
-                Ellipse(pos=(self.width * x, self.height * y), size=(self.width * 0.02, self.height * 0.02))
-    
 
 def hex_to_rgb(hex_color):
     """
     Takes a hex color code and returns a tuple of RGB values from 0 to 1
     """
     hex_color = hex_color.lstrip('#')
-    return tuple(int(hex_color[i:i+2], 16) / 255 for i in (0, 2, 4))
+    return tuple([int(hex_color[i:i+2], 16) for i in (0, 2, 4)] + [255])
 
 if __name__ == '__main__':
     MyApp().run()
